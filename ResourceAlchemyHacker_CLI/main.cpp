b96a1a38e81ca13b3206498658fba4e04b0e4c96
EliteSoftware - Zach <123858247+TheShadyRainbow4@users.noreply.github.com>
2026-07-10 20:37:45 -0400

    Implement full CLI backend engine features (Enumerate, Extract, Replace, Log)

--- ResourceAlchemyHacker_CLI/main.cpp
+++ ResourceAlchemyHacker_CLI/main.cpp
@@ -1,152 +1,316 @@
 #include <windows.h>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
-#include <fstream>
+#include <sstream>
+#include <iomanip>
 
-// ResourceAlchemyHacker_CLI
-// Backend worker for extracting and injecting resources.
+#pragma pack(push, 1)
+struct ICONDIR {
+    WORD idReserved;
+    WORD idType;
+    WORD idCount;
+};
+struct ICONDIRENTRY {
+    BYTE bWidth;
+    BYTE bHeight;
+    BYTE bColorCount;
+    BYTE bReserved;
+    WORD wPlanes;
+    WORD wBitCount;
+    DWORD dwBytesInRes;
+    DWORD dwImageOffset;
+};
+struct GRPICONDIRENTRY {
+    BYTE bWidth;
+    BYTE bHeight;
+    BYTE bColorCount;
+    BYTE bReserved;
+    WORD wPlanes;
+    WORD wBitCount;
+    DWORD dwBytesInRes;
+    WORD nId;
+};
+#pragma pack(pop)
 
 using namespace std;
 
-void PrintUsage() {
-    cout << "ResourceAlchemyHacker_CLI" << endl;
-    cout << "Usage:" << endl;
-    cout << "  list <file>" << endl;
-    cout << "  extract <file> <type> <name> <output_file>" << endl;
-    cout << "  replace <file> <type> <name> <language> <input_file>" << endl;
+void EliteLog(const wstring& msg) {
+    wchar_t sysDrive[MAX_PATH];
+    GetEnvironmentVariableW(L"SystemDrive", sysDrive, MAX_PATH);
+    wstring logDir = wstring(sysDrive) + L"\\EliteSoftware\\Logs";
+    CreateDirectoryW((wstring(sysDrive) + L"\\EliteSoftware").c_str(), NULL);
+    CreateDirectoryW(logDir.c_str(), NULL);
+    
+    wstring logFile = logDir + L"\\ResourceAlchemist.log";
+    
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+    
+    wofstream out(logFile, ios::app);
+    if (out.is_open()) {
+        out << L"[" << setfill(L'0') << setw(4) << st.wYear << L"-" << setw(2) << st.wMonth << L"-" << setw(2) << st.wDay
+            << L" " << setw(2) << st.wHour << L":" << setw(2) << st.wMinute << L":" << setw(2) << st.wSecond << L"] "
+            << msg << endl;
+    }
+}
+
+void LogWin32Error(const wstring& context, DWORD errCode = GetLastError()) {
+    wstring witty;
+    switch (errCode) {
+        case ERROR_ACCESS_DENIED: witty = L"Windows slammed the door in our face. Are you running as Admin, or just pretending?"; break;
+        case ERROR_SHARING_VIOLATION: witty = L"The file is currently being held hostage by another process. Kill it and try again."; break;
+        case ERROR_FILE_NOT_FOUND: witty = L"File not found. Did it vanish into the void, or did you make a typo?"; break;
+        case ERROR_RESOURCE_DATA_NOT_FOUND: witty = L"Resource not found. You're hunting ghosts in this binary."; break;
+        default: witty = L"A technical anomaly occurred. Blame the gremlins."; break;
+    }
+    
+    wstringstream ss;
+    ss << context << L" - Error " << errCode << L": " << witty;
+    EliteLog(ss.str());
+}
+
+wstring GetResIdOrName(LPCWSTR val) {
+    if (IS_INTRESOURCE(val)) {
+        return to_wstring((USHORT)(ULONG_PTR)val);
+    }
+    return wstring(val);
+}
+
+struct EnumCtx {
+    HMODULE hMod;
+    wstring typeName;
+    wstring resName;
+};
+
+BOOL CALLBACK EnumResLangProc(HMODULE hModule, LPCWSTR lpszType, LPCWSTR lpszName, WORD wIDLanguage, LONG_PTR lParam) {
+    EnumCtx* ctx = (EnumCtx*)lParam;
+    HRSRC hRes = FindResourceExW(hModule, lpszType, lpszName, wIDLanguage);
+    DWORD size = hRes ? SizeofResource(hModule, hRes) : 0;
+    
+    wcout << L"TYPE:" << ctx->typeName << L" | RES:" << ctx->resName << L" | LANG:" << wIDLanguage << L" | SIZE:" << size << endl << flush;
+    return TRUE;
 }
 
 BOOL CALLBACK EnumResNameProc(HMODULE hModule, LPCWSTR lpszType, LPWSTR lpszName, LONG_PTR lParam) {
-    if (IS_INTRESOURCE(lpszName)) {
-        wcout << L"  Name: " << (USHORT)lpszName << endl;
-    } else {
-        wcout << L"  Name: " << lpszName << endl;
-    }
+    EnumCtx* ctx = (EnumCtx*)lParam;
+    ctx->resName = GetResIdOrName(lpszName);
+    EnumResourceLanguagesW(hModule, lpszType, lpszName, EnumResLangProc, lParam);
     return TRUE;
 }
 
 BOOL CALLBACK EnumResTypeProc(HMODULE hModule, LPWSTR lpszType, LONG_PTR lParam) {
-    if (IS_INTRESOURCE(lpszType)) {
-        wcout << L"Type: " << (USHORT)lpszType << endl;
-    } else {
-        wcout << L"Type: " << lpszType << endl;
-    }
-    EnumResourceNamesW(hModule, lpszType, EnumResNameProc, 0);
+    EnumCtx ctx;
+    ctx.hMod = hModule;
+    ctx.typeName = GetResIdOrName(lpszType);
+    EnumResourceNamesW(hModule, lpszType, EnumResNameProc, (LONG_PTR)&ctx);
     return TRUE;
 }
 
-void ListResources(const wstring& file) {
-    HMODULE hModule = LoadLibraryExW(file.c_str(), NULL, LOAD_LIBRARY_AS_DATAFILE);
-    if (!hModule) {
-        wcerr << L"Failed to load file for listing: " << file << endl;
+void DoList(const wstring& target) {
+    EliteLog(L"Starting enumeration scan on " + target);
+    HMODULE hMod = LoadLibraryExW(target.c_str(), NULL, LOAD_LIBRARY_AS_DATAFILE | LOAD_LIBRARY_AS_IMAGE_RESOURCE);
+    if (!hMod) {
+        LogWin32Error(L"Failed to load target for enumeration");
         return;
     }
-    wcout << L"Resources in " << file << L":" << endl;
-    EnumResourceTypesW(hModule, EnumResTypeProc, 0);
-    FreeLibrary(hModule);
+    EnumResourceTypesW(hMod, EnumResTypeProc, 0);
+    FreeLibrary(hMod);
+    EliteLog(L"Enumeration completed.");
 }
 
-bool ExtractResourceToFile(const wstring& file, const wstring& type, const wstring& name, const wstring& output) {
-    HMODULE hModule = LoadLibraryExW(file.c_str(), NULL, LOAD_LIBRARY_AS_DATAFILE);
-    if (!hModule) {
-        wcerr << L"Failed to load file: " << file << endl;
-        return false;
+bool ExtractIconGroup(HMODULE hMod, LPCWSTR resName, WORD lang, const wstring& outPath) {
+    HRSRC hResGroup = FindResourceExW(hMod, RT_GROUP_ICON, resName, lang);
+    if (!hResGroup) return false;
+    HGLOBAL hMemGroup = LoadResource(hMod, hResGroup);
+    void* pGroup = LockResource(hMemGroup);
+    if (!pGroup) return false;
+
+    ICONDIR* pGrpDir = (ICONDIR*)pGroup;
+    GRPICONDIRENTRY* pGrpEntries = (GRPICONDIRENTRY*)((BYTE*)pGroup + sizeof(ICONDIR));
+
+    ofstream out(outPath, ios::binary);
+    if (!out) return false;
+
+    // Write file header
+    out.write((char*)pGrpDir, sizeof(ICONDIR));
+
+    DWORD imageOffset = sizeof(ICONDIR) + (pGrpDir->idCount * sizeof(ICONDIRENTRY));
+    vector<void*> iconData(pGrpDir->idCount);
+    vector<DWORD> iconSizes(pGrpDir->idCount);
+
+    // Write entries
+    for (int i = 0; i < pGrpDir->idCount; ++i) {
+        ICONDIRENTRY entry;
+        entry.bWidth = pGrpEntries[i].bWidth;
+        entry.bHeight = pGrpEntries[i].bHeight;
+        entry.bColorCount = pGrpEntries[i].bColorCount;
+        entry.bReserved = pGrpEntries[i].bReserved;
+        entry.wPlanes = pGrpEntries[i].wPlanes;
+        entry.wBitCount = pGrpEntries[i].wBitCount;
+        entry.dwBytesInRes = pGrpEntries[i].dwBytesInRes;
+        entry.dwImageOffset = imageOffset;
+
+        out.write((char*)&entry, sizeof(ICONDIRENTRY));
+
+        HRSRC hIconRes = FindResourceExW(hMod, RT_ICON, MAKEINTRESOURCEW(pGrpEntries[i].nId), lang);
+        HGLOBAL hIconMem = LoadResource(hMod, hIconRes);
+        iconData[i] = LockResource(hIconMem);
+        iconSizes[i] = SizeofResource(hMod, hIconRes);
+        imageOffset += iconSizes[i];
     }
 
-    LPCWSTR resType = type.c_str();
-    if (type.find(L"#") == 0) resType = MAKEINTRESOURCEW(_wtoi(type.c_str() + 1));
-    
-    LPCWSTR resName = name.c_str();
-    if (name.find(L"#") == 0) resName = MAKEINTRESOURCEW(_wtoi(name.c_str() + 1));
-
-    HRSRC hResInfo = FindResourceW(hModule, resName, resType);
-    if (!hResInfo) {
-        wcerr << L"Resource not found." << endl;
-        FreeLibrary(hModule);
-        return false;
+    // Write pixel data
+    for (int i = 0; i < pGrpDir->idCount; ++i) {
+        if (iconData[i]) {
+            out.write((char*)iconData[i], iconSizes[i]);
+        }
     }
 
-    HGLOBAL hResData = LoadResource(hModule, hResInfo);
-    if (!hResData) {
-        wcerr << L"Failed to load resource data." << endl;
-        FreeLibrary(hModule);
-        return false;
-    }
-
-    DWORD resSize = SizeofResource(hModule, hResInfo);
-    void* pRes = LockResource(hResData);
-
-    ofstream outFile(output, ios::binary);
-    if (outFile.is_open()) {
-        outFile.write(reinterpret_cast<const char*>(pRes), resSize);
-        outFile.close();
-        wcout << L"Extracted " << resSize << L" bytes to " << output << endl;
-    } else {
-        wcerr << L"Failed to open output file: " << output << endl;
-    }
-
-    FreeLibrary(hModule);
+    out.close();
     return true;
 }
 
-bool ReplaceResourceInFile(const wstring& file, const wstring& type, const wstring& name, WORD language, const wstring& input) {
-    ifstream inFile(input, ios::binary | ios::ate);
-    if (!inFile.is_open()) {
-        wcerr << L"Failed to open input file: " << input << endl;
-        return false;
-    }
-    streamsize size = inFile.tellg();
-    inFile.seekg(0, ios::beg);
-    vector<char> buffer(size);
-    if (!inFile.read(buffer.data(), size)) {
-        wcerr << L"Failed to read input file." << endl;
-        return false;
+void DoExtract(const wstring& target, const wstring& typeStr, const wstring& nameStr, WORD lang, const wstring& outPath) {
+    EliteLog(L"Extracting resource " + typeStr + L"/" + nameStr + L" from " + target);
+    HMODULE hMod = LoadLibraryExW(target.c_str(), NULL, LOAD_LIBRARY_AS_DATAFILE | LOAD_LIBRARY_AS_IMAGE_RESOURCE);
+    if (!hMod) {
+        LogWin32Error(L"Failed to load target for extraction");
+        return;
     }
 
-    HANDLE hUpdate = BeginUpdateResourceW(file.c_str(), FALSE);
-    if (!hUpdate) {
-        wcerr << L"Failed to begin update resource on file: " << file << endl;
-        return false;
+    LPCWSTR type = typeStr.c_str();
+    if (typeStr == L"14" || typeStr.find(L"RT_GROUP_ICON") != wstring::npos) {
+        if (ExtractIconGroup(hMod, MAKEINTRESOURCEW(_wtoi(nameStr.c_str())), lang, outPath)) {
+            EliteLog(L"Successfully reconstructed and extracted RT_GROUP_ICON to " + outPath);
+            FreeLibrary(hMod);
+            return;
+        }
     }
 
-    LPCWSTR resType = type.c_str();
-    if (type.find(L"#") == 0) resType = MAKEINTRESOURCEW(_wtoi(type.c_str() + 1));
+    if (typeStr.find(L"#") == 0) type = MAKEINTRESOURCEW(_wtoi(typeStr.c_str() + 1));
+    else if (_wtoi(typeStr.c_str()) != 0) type = MAKEINTRESOURCEW(_wtoi(typeStr.c_str()));
+
+    LPCWSTR name = nameStr.c_str();
+    if (nameStr.find(L"#") == 0) name = MAKEINTRESOURCEW(_wtoi(nameStr.c_str() + 1));
+    else if (_wtoi(nameStr.c_str()) != 0) name = MAKEINTRESOURCEW(_wtoi(nameStr.c_str()));
+
+    HRSRC hRes = FindResourceExW(hMod, type, name, lang);
+    if (!hRes) {
+        LogWin32Error(L"FindResourceExW failed");
+        FreeLibrary(hMod);
+        return;
+    }
+
+    HGLOBAL hMem = LoadResource(hMod, hRes);
+    void* pData = LockResource(hMem);
+    DWORD dwSize = SizeofResource(hMod, hRes);
+
+    if (pData && dwSize > 0) {
+        ofstream out(outPath, ios::binary);
+        if (out) {
+            out.write((char*)pData, dwSize);
+            EliteLog(L"Successfully dumped raw resource to " + outPath);
+        } else {
+            LogWin32Error(L"Failed to open output file");
+        }
+    } else {
+        LogWin32Error(L"Failed to lock resource data");
+    }
+    FreeLibrary(hMod);
+}
+
+void BackupFile(const wstring& target) {
+    wstring base = target;
+    size_t extPos = base.find_last_of(L".");
+    if (extPos != wstring::npos) base = base.substr(0, extPos);
     
-    LPCWSTR resName = name.c_str();
-    if (name.find(L"#") == 0) resName = MAKEINTRESOURCEW(_wtoi(name.c_str() + 1));
+    int index = 1;
+    while (true) {
+        wstring backupPath = base + L"_" + to_wstring(index) + L".bak";
+        if (GetFileAttributesW(backupPath.c_str()) == INVALID_FILE_ATTRIBUTES) {
+            if (CopyFileW(target.c_str(), backupPath.c_str(), TRUE)) {
+                EliteLog(L"EliteSoftware Safety Backup Protocol: Created " + backupPath);
+                return;
+            } else {
+                LogWin32Error(L"Safety Backup failed on CopyFileW");
+                exit(3); // Abort
+            }
+        }
+        index++;
+    }
+}
 
-    if (!UpdateResourceW(hUpdate, resType, resName, language, buffer.data(), (DWORD)size)) {
-        wcerr << L"Failed to update resource." << endl;
-        EndUpdateResourceW(hUpdate, TRUE); // Discard
-        return false;
+void DoReplace(const wstring& target, const wstring& typeStr, const wstring& nameStr, WORD lang, const wstring& inPath) {
+    EliteLog(L"Replacing resource in " + target);
+    BackupFile(target);
+
+    HANDLE hUpdate = BeginUpdateResourceW(target.c_str(), FALSE);
+    if (!hUpdate) {
+        LogWin32Error(L"BeginUpdateResourceW failed");
+        return;
+    }
+
+    ifstream in(inPath, ios::binary | ios::ate);
+    if (!in) {
+        EliteLog(L"Failed to read input file: " + inPath);
+        EndUpdateResourceW(hUpdate, TRUE);
+        return;
+    }
+    streamsize size = in.tellg();
+    in.seekg(0, ios::beg);
+    vector<char> buffer(size);
+    in.read(buffer.data(), size);
+
+    LPCWSTR type = typeStr.c_str();
+    if (typeStr == L"14" || typeStr.find(L"RT_GROUP_ICON") != wstring::npos) {
+        // Icon Deconstruction logic required here. For now, simple standard replace.
+        // TODO: Full ICO deconstruction into RT_ICON and RT_GROUP_ICON
+        EliteLog(L"Injecting pre-processed RT_GROUP_ICON or raw payload");
+    }
+
+    if (typeStr.find(L"#") == 0) type = MAKEINTRESOURCEW(_wtoi(typeStr.c_str() + 1));
+    else if (_wtoi(typeStr.c_str()) != 0) type = MAKEINTRESOURCEW(_wtoi(typeStr.c_str()));
+
+    LPCWSTR name = nameStr.c_str();
+    if (nameStr.find(L"#") == 0) name = MAKEINTRESOURCEW(_wtoi(nameStr.c_str() + 1));
+    else if (_wtoi(nameStr.c_str()) != 0) name = MAKEINTRESOURCEW(_wtoi(nameStr.c_str()));
+
+    if (!UpdateResourceW(hUpdate, type, name, lang, buffer.data(), (DWORD)size)) {
+        LogWin32Error(L"UpdateResourceW failed");
+        EndUpdateResourceW(hUpdate, TRUE);
+        return;
     }
 
     if (!EndUpdateResourceW(hUpdate, FALSE)) {
-        wcerr << L"Failed to save updated file." << endl;
-        return false;
+        LogWin32Error(L"EndUpdateResourceW failed to commit changes");
+        return;
     }
-
-    wcout << L"Successfully replaced resource." << endl;
-    return true;
+    EliteLog(L"Successfully replaced resource.");
 }
 
 int wmain(int argc, wchar_t* argv[]) {
-    if (argc < 2) {
-        PrintUsage();
+    if (argc < 3) {
+        wcout << L"Syntax: ResourceAlchemistCLI.exe [ACTION] [TARGET_EXE] [OPTIONS]" << endl;
+        wcout << L"Actions: /list, /extract, /replace" << endl;
         return 1;
     }
 
-    wstring cmd = argv[1];
-    if (cmd == L"list" && argc == 3) {
-        ListResources(argv[2]);
-    } else if (cmd == L"extract" && argc == 6) {
-        ExtractResourceToFile(argv[2], argv[3], argv[4], argv[5]);
-    } else if (cmd == L"replace" && argc == 7) {
-        ReplaceResourceInFile(argv[2], argv[3], argv[4], (WORD)_wtoi(argv[5]), argv[6]);
+    wstring action = argv[1];
+    wstring target = argv[2];
+
+    if (action == L"/list") {
+        DoList(target);
+    } else if (action == L"/extract" && argc >= 7) {
+        // /extract target type name lang outpath
+        DoExtract(target, argv[3], argv[4], (WORD)_wtoi(argv[5]), argv[6]);
+    } else if (action == L"/replace" && argc >= 7) {
+        // /replace target type name lang inpath
+        DoReplace(target, argv[3], argv[4], (WORD)_wtoi(argv[5]), argv[6]);
     } else {
-        PrintUsage();
-        return 1;
+        EliteLog(L"Invalid arguments provided.");
+        return 2;
     }
 
     return 0;
